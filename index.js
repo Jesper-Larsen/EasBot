849e793018496450422280949100afab73a440c3
JonesTheG <4aflarsen@familie.tele.dk>
2022-01-21 13:16:51 +0100

    added slash commands handler and permissions

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const { Client } = require("discord.js");
+const { Client, Collection } = require("discord.js");
 const client = new Client({intents: 32767});
+
+client.commands = new Collection()
 
 // makes hidden .env file readable and loads the env file
 require('dotenv').config();
+
 require("./Handlers/Events")(client);
+require("./Handlers/Commands")(client);
 
 
 client.login(process.env.TOKEN);
